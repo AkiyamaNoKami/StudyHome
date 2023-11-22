96154c78d4a7bccd2d48342039911751cbfd429b
AmaneTsu <amanetsukasa724@gmail.com>
2023-11-22 22:10:46 +0300

    Create some urls and basic views. Fix jQuery connection problem

--- school/urls.py
+++ school/urls.py
@@ -4,5 +4,11 @@ from . import views
 app_name='school'
 
 urlpatterns = [
+    path('about/', views.about, name='about'),
+    path('contact/', views.contact, name='contact'),
+    path('courses/', views.courses, name='courses'),
+    path('team/', views.team, name='team'),
+    path('testimonial/', views.testimonial, name='testimonial'),
     path('', views.product_all, name='school_home'),
+
 ]
--- school/views.py
+++ school/views.py
@@ -2,3 +2,18 @@ from django.shortcuts import render
 
 def product_all(request):
     return render(request, 'school/index.html')
+
+def about(request):
+    return render(request, 'school/about.html')
+
+def contact(request):
+    return render(request, 'school/contact.html')
+
+def courses(request):
+    return render(request, 'school/courses.html')
+
+def team(request):
+    return render(request, 'school/team.html')
+
+def testimonial(request):
+    return render(request, 'school/testimonial.html')
