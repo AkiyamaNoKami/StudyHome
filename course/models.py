827994b814af094b417e5aace3a5c4eb55ef741b
AmaneTsu <amanetsukasa724@gmail.com>
2023-11-26 00:40:40 +0300

    Editing models and create db for tests

--- course/models.py
+++ course/models.py
@@ -1,44 +1,49 @@
-# from django.db import models
-# from teacher.models import Teacher
-# from student.models import Student
-#
-#
-#
-# class Subject(models.Model):
-#     subject_id = models.AutoField(primary_key=True)
-#     title = models.CharField(max_length=30)
-#
-#     def __str__(self):
-#         return self.title
-#
-# class CourseType(models.Model):
-#     type = models.CharField(max_length=50)
-#
-#     def __str__(self):
-#         return self.type
-#
-# class Course(models.Model):
-#     course_id = models.AutoField(primary_key=True)
-#     type = models.ForeignKey(CourseType, on_delete=models.SET_NULL, null=True, blank=True)
-#     subject = models.ForeignKey(Subject, on_delete=models.SET_NULL, null=True, blank=True)
-#     title = models.CharField(max_length=50)
-#     duration = models.IntegerField(max_length=4)
-#     cost = models.DecimalField(max_digits=10, decimal_places=2)
-#     description = models.TextField()
-#
-#
-# class Mark(models.Model):
-#     mark_id = models.AutoField(primary_key=True)
-#     score = models.IntegerField(max_length=1)
-#
-#
-# class Lesson(models.Model):
-#     lesson_id = models.AutoField(primary_key=True)
-#     title = models.CharField(max_length=50)
-#     course = models.ForeignKey(Course, on_delete=models.SET_NULL, null=True, blank=True)
-#     teacher = models.ForeignKey(Teacher, on_delete=models.SET_NULL, null=True, blank=True)
-#     mark = models.ForeignKey(Mark, on_delete=models.SET_NULL, null=True, blank=True)
-#     date = models.DateTimeField(editable=True)
-#     duration = models.IntegerField(max_length=3)
-#     students = models.ManyToManyField(Student, blank=True)
-#
+from django.db import models
+from student.models import Student
+
+
+
+class Subject(models.Model):
+    subject_id = models.AutoField(primary_key=True)
+    title = models.CharField(max_length=30)
+
+    def __str__(self):
+        return self.title
+
+
+class CourseType(models.Model):
+    type = models.CharField(max_length=50)
+
+    def __str__(self):
+        return self.type
+
+
+class Course(models.Model):
+    course_id = models.AutoField(primary_key=True)
+    type = models.ForeignKey(CourseType, on_delete=models.SET_NULL, null=True, blank=True)
+    subject = models.ForeignKey(Subject, on_delete=models.SET_NULL, null=True, blank=True)
+    title = models.CharField(max_length=50)
+    duration = models.IntegerField()
+    cost = models.DecimalField(max_digits=10, decimal_places=2)
+    description = models.TextField()
+    students = models.ManyToManyField('student.Student', blank=True, related_name='courses_students')
+
+
+class Lesson(models.Model):
+    lesson_id = models.AutoField(primary_key=True)
+    title = models.CharField(max_length=50)
+    course = models.ForeignKey(Course, on_delete=models.SET_NULL, null=True, blank=True)
+    teacher = models.ForeignKey('teacher.Teacher', on_delete=models.SET_NULL, null=True, blank=True)
+    date = models.DateTimeField(editable=True)
+    duration = models.IntegerField()
+
+class Mark(models.Model):
+    mark_id = models.AutoField(primary_key=True)
+    score = models.IntegerField()
+    student = models.ForeignKey(Student, on_delete=models.CASCADE)
+    lesson = models.ForeignKey(Lesson, on_delete=models.CASCADE)
+
+
+
+
+
--- student/models.py
+++ student/models.py
@@ -1,30 +1,28 @@
-# from django.db import models
-# from course.models import Course, Subject
-#
-# class Student(models.Model):
-#
-#     GENDER_CHOICES = [
-#         ('М', 'Мужской'),
-#         ('Ж', 'Женский'),
-#     ]
-#
-#     student_id = models.AutoField(primary_key=True)
-#     name = models.CharField(max_length=30, blank=False)
-#     surname = models.CharField(max_length=30, blank=False)
-#     last_name = models.CharField(max_length=30, blank=False)
-#     age = models.IntegerField(max_length=3)
-#     gender = models.CharField(max_length=1, choices=GENDER_CHOICES)
-#     parents_phone = models.CharField(max_length=100)
-#     parents_email = models.EmailField(max_length=100, blank=False)
-#     course = models.ForeignKey(Course, on_delete=models.SET_NULL, null=True, blank=True)
-#     phone = models.CharField(max_length=20)
-#     email = models.EmailField(max_length=100, blank=False)
-#     description = models.TextField()
-#
-#     def __str__(self):
-#         return f"{self.surname} {self.name} {self.last_name}"
-#
-#     class Meta:
-#         ordering = ['surname', 'name', 'last_name']
-#
-#
+from django.db import models
+
+
+class Student(models.Model):
+
+    GENDER_CHOICES = [
+        ('М', 'Мужской'),
+        ('Ж', 'Женский'),
+    ]
+
+    student_id = models.AutoField(primary_key=True)
+    name = models.CharField(max_length=30, blank=False)
+    surname = models.CharField(max_length=30, blank=False)
+    last_name = models.CharField(max_length=30, blank=False)
+    age = models.IntegerField()
+    gender = models.CharField(max_length=1, choices=GENDER_CHOICES)
+    parents_phone = models.CharField(max_length=100)
+    parents_email = models.EmailField(max_length=100, blank=False)
+    course = models.ManyToManyField('course.Course', blank=True, related_name='students_courses')
+    phone = models.CharField(max_length=20)
+    email = models.EmailField(max_length=100, blank=False)
+    description = models.TextField()
+
+    def __str__(self):
+        return f"{self.surname} {self.name} {self.last_name}"
+
+    class Meta:
+        ordering = ['surname', 'name', 'last_name']
