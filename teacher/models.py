bcae755201456a89e6b35bc007520f9cf201a740
AmaneTsu <amanetsukasa724@gmail.com>
2023-11-25 20:03:12 +0300

    Create teacher db model

--- teacher/models.py
+++ teacher/models.py
@@ -1,3 +1,37 @@
 from django.db import models
 
-# Create your models here.
+
+class Subject(models.Model):
+    subject_id = models.AutoField(primary_key=True)
+    title = models.CharField(max_length=30)
+
+    def __str__(self):
+        return self.title
+class Teacher(models.Model):
+
+    GENDER_CHOICES = [
+        ('М', 'Мужской'),
+        ('Ж', 'Женский'),
+    ]
+
+    teacher_id = models.AutoField(primary_key=True)
+    name = models.CharField(max_length=30, blank=False)
+    surname = models.CharField(max_length=30, blank=False)
+    last_name = models.CharField(max_length=30, blank=False)
+    age = models.IntegerField(max_length=3)
+    gender = models.CharField(max_length=1, choices=GENDER_CHOICES)
+    subject = models.ForeignKey(Subject, on_delete=models.SET_NULL, null=True, blank=True)
+    job = models.CharField(max_length=30)
+    education = models.CharField(max_length=100)
+    experience = models.IntegerField(max_length=2)
+    phone = models.CharField(max_length=20)
+    email = models.EmailField(max_length=100, blank=False)
+    description = models.TextField()
+
+    def __str__(self):
+        return f"{self.surname} {self.name} {self.last_name}"
+
+    class Meta:
+        ordering = ['surname', 'name', 'last_name']
+
+
