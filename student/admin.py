9e125bea1594cb16156e6cf5e55d9c2a349283d0
AmaneTsu <amanetsukasa724@gmail.com>
2023-11-27 03:48:36 +0300

    Try to fix db problem

--- student/admin.py
+++ student/admin.py
@@ -1,4 +1,14 @@
 from django.contrib import admin
+from django.db import models
+from django.contrib.admin.widgets import FilteredSelectMultiple
 from .models import Student
+
+class StudentAdmin(admin.ModelAdmin):
+    list_display = ['surname', 'name', 'last_name', 'age', 'gender', 'parents_phone', 'parents_email', 'phone', 'email', 'description']
+    filter_vertical = ['course',]
+    formfield_overrides = {
+        models.ManyToManyField: {'widget': FilteredSelectMultiple('Course', is_stacked=False)},
+    }
+
 
 admin.site.register(Student)
