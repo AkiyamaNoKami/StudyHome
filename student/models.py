974add6bf523fcaa18b31c1d3313614688ffdf5d
AmaneTsu <amanetsukasa724@gmail.com>
2023-11-27 03:01:29 +0300

    Setting up img field in models. Editing html template

--- student/models.py
+++ student/models.py
@@ -20,7 +20,7 @@ class Student(models.Model):
     phone = models.CharField(max_length=20)
     email = models.EmailField(max_length=100, blank=False)
     description = models.TextField()
-    img = models.ImageField(upload_to='images/')
+    img = models.ImageField(upload_to='images/', blank=True)
 
     def __str__(self):
         return f"{self.surname} {self.name} {self.last_name}"
