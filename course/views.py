2cc74c0df2dbf31d72a0d3fdb43f4ae3af4ab3ee
AmaneTsu <amanetsukasa724@gmail.com>
2023-12-05 20:08:47 +0300

    Complete base part of templates and create new account app

--- course/views.py
+++ course/views.py
@@ -1,7 +1,4 @@
 from django.shortcuts import render
-from .models import Course
 
 def course(requets):
-    course = Course.objects.all()
-    context = {'course':course}
-    return render(requets, 'courses/courses.html', context)
+    return render(requets, 'courses/courses.html')
