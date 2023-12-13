5cebf3db224ddbfc51eefdd6de1e7509e6a801cf
AmaneTsu <amanetsukasa724@gmail.com>
2023-12-13 22:33:42 +0300

    Complete output students without homework

--- teacher_personal/views.py
+++ teacher_personal/views.py
@@ -1,7 +1,7 @@
 from django.shortcuts import render, redirect
 from .forms import GradeForm
 from django.shortcuts import get_object_or_404
-from course.models import Lesson, Homework, StudentHomework
+from course.models import Lesson, Homework, StudentHomework, Student, Course
 from account.models import Account
 from django.contrib.auth.decorators import login_required
 from django.contrib import messages
@@ -30,11 +30,26 @@ def teacher_homework_view(request, lesson_id):
         if account.teacher:
             teacher = account.teacher
             lessons = Lesson.objects.filter(pk=lesson_id, teacher=teacher)
+            ls = Lesson.objects.get(pk=lesson_id, teacher=teacher)
+            homeworks = StudentHomework.objects.filter(homework__lesson=ls)
+            student_homeworks = {}
+            course = ls.course
+            students_in_lesson = Student.objects.filter(course=course)
+
+            for student in students_in_lesson:
+                student_hw = 1 if homeworks.filter(student=student) else 0
+                student_homeworks[student] = student_hw
+
+            if not student_homeworks:
+                messages.warning(request, 'Нет студентов на данном уроке.')
+                return redirect('teacher_personal:teacher_homeworks', lesson_id=lesson_id)
+            student_without_homework = [student for student, value in student_homeworks.items() if value == 0]
+
             if lessons.exists():
                 lesson = lessons.first()
                 homeworks = StudentHomework.objects.filter(homework__lesson=lesson)
                 total_homework = Homework.objects.filter(lesson=lesson).first()
-                context = {'homeworks': homeworks, 'total_homework': total_homework, 'lesson': lesson}
+                context = {'homeworks': homeworks, 'total_homework': total_homework, 'lesson': lesson, 'student_homeworks': student_homeworks, 'students_in_lesson': students_in_lesson, 'student_without_homework': student_without_homework}
                 return render(request, 'teacher_pm/teacher_homework.html', context)
         else:
             return redirect('account:login')
@@ -49,25 +64,29 @@ def update_grade_view(request, lesson_id):
         if account.teacher:
             teacher = account.teacher
             lesson = Lesson.objects.get(pk=lesson_id, teacher=teacher)
+            students = Student.objects.all()
 
             if request.method == 'POST':
                 form = GradeForm(request.POST)
                 if form.is_valid():
                     homework_id = form.cleaned_data['homework_id']
                     grade = form.cleaned_data['grade']
-
-                    # homework_id = request.POST.get('homework_id')
-                    # grade = request.POST.get('grade')
-
                     student_homework = get_object_or_404(StudentHomework, pk=homework_id)
                     student_homework.total_grade = grade
                     student_homework.save()
 
                     return redirect('teacher_personal:teacher_homeworks', lesson_id=lesson_id)
-                else:
-                    messages.error(request, 'Пожалуйста введите корректные значения, оценка должна быть от 0 до 5')
             else:
                 form = GradeForm()
-            return render(request, 'teacher_pm/teacher_homework.html', {'lesson': lesson})
+
+            context = {
+                'lesson': lesson,
+                'students': students,
+                # 'homeworks': homeworks,
+                'form': form,
+                # 'student_homeworks': student_homeworks,
+            }
+
+            return render(request, 'teacher_pm/teacher_homework.html', context)
     except Account.DoesNotExist:
         return redirect('account:login')
